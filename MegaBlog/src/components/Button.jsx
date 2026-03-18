0e1db7a2be86b7f7cc5f49bb5e0190726abb3dd6
Sumit12827 <sumitdowney@gmail.com>
2026-03-18 23:06:57 +0530

    completeComponents

--- MegaBlog/src/components/Button.jsx
+++ MegaBlog/src/components/Button.jsx
@@ -11,7 +11,7 @@ function Button({
 
 }) {
     return (
-        <button className= {`px-4 py-2 rounded-lg  ${bgColor} ${className} ${textColor}`} >{children}</button>
+        <button className= {`px-4 py-2 rounded-lg  ${bgColor} ${className} ${textColor}`} {...props} > {children} </button>
     )
 }
 
