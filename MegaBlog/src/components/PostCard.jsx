985dbe48941000f5e8e237a4deb0414eff901e71
Sumit12827 <sumitdowney@gmail.com>
2026-03-19 13:03:44 +0530

    using react hook forms

---
+++ MegaBlog/src/components/PostCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import appwriteService from "../services/appwriteService";
+import { Link } from "react-router-dom";
+
+function PostCard({
+    $id , title , featuredImage 
+}) {
+    return (
+    <Link to =  {`/Post/${$id}`}>  
+     <div className='w-full bg-gray-100 rounded-xl p-4'>
+        <div className='w-full justify centre mb-4'>
+
+         <img src = {appwriteService.getFilePreview(featuredImage)} alt = {title} className="rounded-xl" />
+
+        </div>
+        <h2 className="">{title}</h2>
+     </div>
+    </Link>
+    )
+}
---
+++ MegaBlog/src/components/Select.jsx
@@ -0,0 +1,32 @@
+import React  , {useId} from "react"; 
+
+function Select({
+    options ,
+    label ,
+    className ,
+    ...props
+
+} , ref) {
+    const id = useId();
+    return(
+     <div className="w-full "> {label && <label htmlFor= {id} className=" "></label>}
+     <select
+        {...props}
+        id = {id}
+        ref = {ref}
+        className = {`px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-full ${className}`}
+     >
+
+       {options?.map((option) => (
+
+        <option key = {option} value = {option}>
+            {option}
+        </option>
+       ))}
+
+     </select>
+     </div>
+    )
+}
+
+export default React.forwardRef(Select);
